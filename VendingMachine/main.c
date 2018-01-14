53a0cd21d78871a4b4ec77ab374e25ab5eb243e9
sirhamsteralot <sirhamsteralot@gmail.com>
2018-01-14 12:53:42 +0100

    New PC

--- VendingMachine/main.c
+++ VendingMachine/main.c
@@ -21,7 +21,7 @@ typedef enum { E_10C, E_20C, E_50C, E_100C, E_Not_Enough_Money, E_Enough_Money, 
 event_t;
 //your states
 typedef enum { S_Idle, S_Initialisation, S_Wait_For_Selection, S_Wait_For_Money, S_Amount_Check, S_Admin_Mode, S_Wait_For_Admin_Selection
-}
+} 
 state_t;
 
 state_t event_handler(event_t event);
