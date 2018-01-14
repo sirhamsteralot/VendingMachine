8a85f36fedcb9efd45f504e2b2e2528e7abad6db
sirhamsteralot <sirhamsteralot@gmail.com>
2018-01-14 13:30:19 +0100

    Comments

--- VendingMachine/main.c
+++ VendingMachine/main.c
@@ -17,7 +17,7 @@ int Dishwater_Stock;
 int Change_Stock;
 
 // your events
-typedef enum { E_10C, E_20C, E_50C, E_100C, E_Not_Enough_Money, E_Enough_Money, E_Start, E_Continue, E_Coffe, E_Mokka, E_Choco, E_DishwasherWater, E_OutOfStock, E_AdminMode, E_Restock, E_EmptyChange, E_ExitAdmin}
+typedef enum { E_10C, E_20C, E_50C, E_100C, E_Not_Enough_Money, E_Enough_Money, E_Start, E_Continue, E_Coffe, E_Mokka, E_Choco, E_DishwasherWater, E_OutOfStock, E_AdminMode, E_Restock, E_EmptyChange, E_AddChange100, E_ExitAdmin}
 event_t;
 //your states
 typedef enum { S_Idle, S_Initialisation, S_Wait_For_Selection, S_Wait_For_Money, S_Amount_Check, S_Admin_Mode, S_Wait_For_Admin_Selection
@@ -42,7 +42,7 @@ void ReadFromFIle();
 int Price = 125;
 char drinkName[255];
 
-//**************************************** main program
+/*===============| main program |===============*/
 
 int main()
 {
@@ -51,9 +51,7 @@ int main()
 	return 0;
 }
 
-//**************************************** functions
-
-
+/*================| functions |================*/
 
 state_t event_handler(event_t event) {
 	static state_t state = S_Idle;
@@ -61,7 +59,7 @@ state_t event_handler(event_t event) {
 	state_t next_state;
 
 	while (1) {
-		// *********** Source States/Events *************
+		// Source States/Events
 		switch (state) {
 		case S_Idle:
 			switch (event) {
@@ -165,9 +163,11 @@ state_t event_handler(event_t event) {
 		case S_Admin_Mode:
 			system("@cls||clear");
 			puts("Welcome to the admin mode");
+			// Default Destination State
 			next_state = S_Wait_For_Admin_Selection;
 			switch (event) {
 			case E_Restock:
+				// Mealy Action
 				Coffee_Stock = 10;
 				Mokka_Stock = 10;
 				Choco_Stock = 10;
@@ -175,19 +175,28 @@ state_t event_handler(event_t event) {
 				WriteToFile();
 				puts("Restocked!");
 				break;
+			case E_AddChange100:
+				// Mealy Action
+				Change_Stock += 100;
+				WriteToFile();
+				puts("Added 100 cents");
+				break;
 			case E_EmptyChange:
+				// Mealy Action
 				Change_Stock = 0;
 				WriteToFile();
 				puts("Emptied change!");
 				break;
 			case E_ExitAdmin:
+				// Mealy Action
 				system("@cls||clear");
+				// Destination State
 				next_state = S_Initialisation;
 				break;
 			}
 			break;
 		}
-		// *********** Destination States *************
+		/*===============| Destination States  |===============*/
 		switch (next_state) {
 		case S_Initialisation:
 			// Moore Action
@@ -196,13 +205,16 @@ state_t event_handler(event_t event) {
 			event = E_Continue;
 			break;
 		case S_Wait_For_Selection:
+			// Moore Action
 			event = Selection();
 			// Event Generation/Preparation
 			event = event;
 			break;
 
 		case S_Wait_For_Admin_Selection:
+			// Moore Action
 			event = AdminModeSelect();
+			// Event Generation/Preparation
 			event = event;
 			next_state = S_Admin_Mode;
 			break;
@@ -226,12 +238,13 @@ state_t event_handler(event_t event) {
 }
 
 
-
+// Initialization
 void initialize(int *money) {
 	*money = 0;
 	ReadFromFIle();
 }
 
+// Coffee dispensing Logic
 void dispense_coffee(const int money) {
 	printf("\n Enough Money!(%d) Thanks \n", money);
 	printf("\n Your Change:%d", money - Price);
@@ -258,6 +271,7 @@ void dispense_coffee(const int money) {
 	system("@cls||clear");
 }
 
+// Money Inserted Logic
 void add_10(int *money) {
 	*money += 10;
 	Change_Stock += 10;
@@ -282,10 +296,12 @@ void add_100(int *money) {
 	WriteToFile();
 }
 
+// Amount Check Event
 event_t check_amount(const int money) {
 	return (money < Price) ? E_Not_Enough_Money : E_Enough_Money;
 }
 
+// Coin Selection interface
 event_t coin_insertion() {
 	int sel;
 	while (1)
@@ -320,6 +336,7 @@ event_t coin_insertion() {
 	}
 }
 
+// Main selection interface
 event_t Selection() {
 	int sel;
 	while (1)
@@ -370,13 +387,15 @@ event_t Selection() {
 	}
 }
 
+//Admin mode selection interface
 event_t AdminModeSelect() {
 	int sel;
 
 	puts("\nSelect your option:");
 	puts("1-- Refill all");
 	puts("2-- Empty change");
-	puts("3-- Exit");
+	puts("3-- Add 100 cents to change");
+	puts("4-- Exit");
 
 	scanf("%d", &sel);
 	getchar();
@@ -388,10 +407,15 @@ event_t AdminModeSelect() {
 		return E_EmptyChange;
 		break;
 	case 3:
+		return E_AddChange100;
+		break;
+	case 4:
 		return E_ExitAdmin;
 		break;
 	}
 }
+
+// Serialization
 
 void WriteToFile() {
 	// INITIALIZE
